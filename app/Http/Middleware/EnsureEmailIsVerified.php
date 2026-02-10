924231ebb580a69d6545b8bb76c291a7d03d7ce0
adebayoisreal122 <adebayoisrealight122@gmail.com>
2026-02-10 12:26:02 +0100

    fixed cors problem

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\Middleware\EnsureEmailIsVerified as Middleware;
+
+class EnsureEmailIsVerified extends Middleware
+{
+ 
+}
